b70f7b99891ef21b1f5f8922cb931924ee5931ef
Drew Croteau <requin@csu.fullerton.edu>
2021-12-11 17:14:10 -0800

    Testing push

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
         </p>
         <ul>
           <li>
-            <Link to="/about/">About</Link>
+            <Link to="/about/">Test</Link>
           </li>
           <li>
             <a href={companyUrl}>
